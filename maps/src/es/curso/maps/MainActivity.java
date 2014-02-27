8fdc3735d74b3d7e5f742bc9126565c043eb8f54
poxtius <poxtius@gmail.com>
2014-02-27 23:44:32 +0100

    maps

--- maps/src/es/curso/maps/MainActivity.java
+++ maps/src/es/curso/maps/MainActivity.java
@@ -5,15 +5,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 public class MainActivity extends Activity {
 
 	//Google Map
